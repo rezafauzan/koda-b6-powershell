bec2619a936aa4b62c7c51fe60c5b873a827a00f
Reza Fauzan Adhima <rezafauzan593@gmail.com>
2025-12-22 20:14:06 +0700

    Hotfix : if compare operator using -eq not "=", Structure of genereated folder changed

--- generate-music.ps1
+++ generate-music.ps1
@@ -6,55 +6,61 @@ $IwanSongs = "Bento.mp3","Bongkar.mp3","Ibu.mp3","Kemesraan.mp3", "Serdadu.mp3"
 $PeterSongs = "Ada Apa Denganmu.mp3", "Menghapus Jejakmu.mp3","Mungkin Nanti.mp3","Semua Tentang Kita.mp3","Separuh Agus.mp3"
 $RoseSongs = "Cinta Dalam Hidupku.mp3","Hati Yang Kau Sakiti.mp3","Hey Ladies.mp3","Hijrah Cinta.mp3","Terlalu Cinta.mp3"
 
-foreach( $artist in $artists){
-    if($artist = "Ahmad Yani"){
+echo "Note: Program Hanya Akan Berjalan Baik Ketika Folder `"Music`" beserta isinya belum ada di cwd "
+
+New-Item -ItemType Directory "Music"
+Set-Location "./Music"
+
+foreach($artist in $artists){
+    if($artist -eq "Ahmad Yani"){
         New-Item -ItemType Directory $artist
         echo "Berhasil membuat folder $artist"
+        Set-Location "./$artist" 
         foreach( $song in $AhmadSongs ){
-            Set-Location "C:\Users\Snowfox\Documents\Bootcamp Koda\Day 1\Minitask 2\$artist" 
             New-Item $song
             echo "Berhasil membuat file $song di folder $artist"
         }
-        Set-Location "C:\Users\Snowfox\Documents\Bootcamp Koda\Day 1\Minitask 2\" 
+        echo "Selesai Membuat Folder beserta isinya"
+        Set-Location ".."
     }
-    if($artist = "Ebiet G. Ada"){
+    if($artist -eq "Ebiet G. Ada"){
         New-Item -ItemType Directory $artist
         echo "Berhasil membuat folder $artist"
+        Set-Location "./$artist"
         foreach( $song in $EbietSongs ){
-            Set-Location "C:\Users\Snowfox\Documents\Bootcamp Koda\Day 1\Minitask 2\$artist" 
             New-Item $song
             echo "Berhasil membuat file $song di folder $artist"
         }
-        Set-Location "C:\Users\Snowfox\Documents\Bootcamp Koda\Day 1\Minitask 2\" 
+        Set-Location ".."
     }
-    if($artist = "Iwan Setiawan"){
+    if($artist -eq "Iwan Setiawan"){
         New-Item -ItemType Directory $artist
         echo "Berhasil membuat folder $artist"
+        Set-Location "./$artist"
         foreach( $song in $IwanSongs ){
-            Set-Location "C:\Users\Snowfox\Documents\Bootcamp Koda\Day 1\Minitask 2\$artist" 
             New-Item $song
             echo "Berhasil membuat file $song di folder $artist"
         }
-        Set-Location "C:\Users\Snowfox\Documents\Bootcamp Koda\Day 1\Minitask 2\" 
+        Set-Location ".."
     }
-    if($artist = "Peter Griffin"){
+    if($artist -eq "Peter Griffin"){
         New-Item -ItemType Directory $artist
         echo "Berhasil membuat folder $artist"
+        Set-Location "./$artist"
         foreach( $song in $PeterSongs ){
-            Set-Location "C:\Users\Snowfox\Documents\Bootcamp Koda\Day 1\Minitask 2\$artist" 
             New-Item $song
             echo "Berhasil membuat file $song di folder $artist"
         }
-        Set-Location "C:\Users\Snowfox\Documents\Bootcamp Koda\Day 1\Minitask 2\" 
+        Set-Location ".."
     }
-    if($artist = "Rose"){
+    if($artist -eq "Rose"){
         New-Item -ItemType Directory $artist
         echo "Berhasil membuat folder $artist"
+        Set-Location "./$artist"
         foreach( $song in $RoseSongs ){
-            Set-Location "C:\Users\Snowfox\Documents\Bootcamp Koda\Day 1\Minitask 2\$artist" 
             New-Item $song
             echo "Berhasil membuat file $song di folder $artist"
         }
-        Set-Location "C:\Users\Snowfox\Documents\Bootcamp Koda\Day 1\Minitask 2\" 
+        Set-Location ".."
     }
 }
